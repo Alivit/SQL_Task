64d6ac2001eca64cc7d2ff4558603f0887205dcd
Alivit <vltal241201@gmail.com>
2023-03-15 19:25:29 +0300

    feat: add seconr sql request

--- resourсes/sql/sql-task.sql
+++ resourсes/sql/sql-task.sql
@@ -1,6 +1,14 @@
--- Вывести к каждому самолету класс обслуживания
--- и количество мест этого класса
+--Вывести к каждому самолету класс обслуживания
+--и количество мест этого класса
 
-SELECT DISTINCT model, fare_conditions, count(*) FROM aircrafts_data
-JOIN seats s on aircrafts_data.aircraft_code = s.aircraft_code
-group by fare_conditions, model
+SELECT DISTINCT model, fare_conditions, count(*) FROM aircrafts_data a
+JOIN seats s on a.aircraft_code = s.aircraft_code
+group by model, fare_conditions;
+
+--Найти 3 самых вместительных самолета (модель + кол-во мест)
+
+SELECT DISTINCT model, count(*) as number_of_seats FROM aircrafts_data a
+JOIN seats s on a.aircraft_code = s.aircraft_code
+group by model
+order by number_of_seats DESC
+limit 3;
