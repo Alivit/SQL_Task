c0cc182218626e5df01b10739598718b293c9052
Alivit <vltal241201@gmail.com>
2023-03-15 20:49:52 +0300

    feat: fourth sql request

--- resourсes/sql/sql-task.sql
+++ resourсes/sql/sql-task.sql
@@ -2,23 +2,29 @@
 --и количество мест этого класса
 
 SELECT DISTINCT model, fare_conditions, count(*) FROM aircrafts_data a
-JOIN seats s on a.aircraft_code = s.aircraft_code
-group by model, fare_conditions;
+JOIN seats s ON a.aircraft_code = s.aircraft_code
+GROUP BY model, fare_conditions;
 
 --Найти 3 самых вместительных самолета (модель + кол-во мест)
 
-SELECT DISTINCT model, count(*) as number_of_seats FROM aircrafts_data a
-JOIN seats s on a.aircraft_code = s.aircraft_code
-group by model
-order by number_of_seats DESC
-limit 3;
+SELECT DISTINCT model, count(*) AS number_of_seats FROM aircrafts_data a
+JOIN seats s ON a.aircraft_code = s.aircraft_code
+GROUP BY model
+ORDER BY number_of_seats DESC
+LIMIT 3;
 
 --Вывести код,модель самолета и места не эконом класса
 --для самолета 'Аэробус A321-200' с сортировкой по местам
 
 SELECT DISTINCT a.aircraft_code, model, seat_no FROM aircrafts_data a
-JOIN seats s on a.aircraft_code = s.aircraft_code
+JOIN seats s ON a.aircraft_code = s.aircraft_code
 WHERE model->>'ru' LIKE '%Аэробус A321-200%' AND fare_conditions NOT LIKE '%Economy%'
-order by seat_no;
+ORDER BY seat_no;
+
+--Вывести города в которых больше 1 аэропорта ( код аэропорта, аэропорт, город)
+
+SELECT city, count(*) FROM airports_data
+GROUP BY city
+HAVING count(*) > 1;
 
 
