0a36825beab2cf844f52ac7dbb054b30a7198c94
Alivit <vltal241201@gmail.com>
2023-03-15 20:24:01 +0300

    feat: add third sql request

--- resourсes/sql/sql-task.sql
+++ resourсes/sql/sql-task.sql
@@ -12,3 +12,13 @@ JOIN seats s on a.aircraft_code = s.aircraft_code
 group by model
 order by number_of_seats DESC
 limit 3;
+
+--Вывести код,модель самолета и места не эконом класса
+--для самолета 'Аэробус A321-200' с сортировкой по местам
+
+SELECT DISTINCT a.aircraft_code, model, seat_no FROM aircrafts_data a
+JOIN seats s on a.aircraft_code = s.aircraft_code
+WHERE model->>'ru' LIKE '%Аэробус A321-200%' AND fare_conditions NOT LIKE '%Economy%'
+order by seat_no;
+
+
